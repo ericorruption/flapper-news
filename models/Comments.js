3947e7b8b408f9009d9ea97c2b59132c1694d3d5
Eric Quanz <eric.quanz@gmail.com>
2015-12-10 16:22:29 -0200

    add upvote method to comments

--- models/Comments.js
+++ models/Comments.js
@@ -13,4 +13,10 @@ var CommentSchema = new mongoose.Schema({
     }
 });
 
+CommentSchema.methods.upvote = function(cb) {
+    this.upvotes += 1;
+    this.save(cb);
+};
+
+
 mongoose.model('Comment', CommentSchema);
--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,18 @@ router.param('post', function(req, res, next, id) {
     });
 });
 
+router.param('comment', function(req, res, next, id) {
+    var query = Comment.findById(id);
+
+    query.exec(function (err, comment) {
+        if (err) { return next(err); }
+        if (!comment) { return next(new Error('can\'t find comment')); }
+
+        req.comment = comment;
+        return next();
+    });
+});
+
 router.get('/posts', function(req, res, next) {
     Post.find(function(err, posts) {
         if (err) { return next(err); }
@@ -45,6 +57,14 @@ router.get('/posts/:post', function(req,res) {
 
 router.put('/posts/:post/upvote', function(req, res, next) {
     req.post.upvote(function(err, post) {
+        if (err) { return next(err); }
+
+        res.json(post);
+    });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+    req.comment.upvote(function(err, post) {
         if (err) { return next(err); }
 
         res.json(post);
