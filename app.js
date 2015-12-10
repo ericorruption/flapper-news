c8f4e035ec11d244ff6d809544e40013fa7d34c1
Eric Quanz <eric.quanz@gmail.com>
2015-12-10 14:29:22 -0200

    send comment behavior / form

--- app.js
+++ app.js
@@ -64,4 +64,15 @@ function($scope, $stateParams, posts) {
 
     $scope.posts = posts.posts[$stateParams.id];
 
+    $scope.addComment = function() {
+        if ($scope.body === '') { return; }
+
+        $scope.post.comments.push({
+            body: $scope.body,
+            author: 'user',
+            upvotes: 0
+        });
+
+        $scope.body = '';
+    }
 }]);
