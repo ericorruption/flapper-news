f8ab41698ab52e3cd3e219b96773f2eff516c073
Eric Quanz <eric.quanz@gmail.com>
2015-12-11 11:18:17 -0200

    add login endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var User = mongoose.model('User');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,6 +25,22 @@ router.post('/register', function(req, res, next) {
 
         return res.json({ token: user.generateJWT() });
     });
+});
+
+router.post('/login', function(req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).json({ message: 'Please fill out all fields' });
+    }
+
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+
+        if (user) {
+            return res.json({ token: user.generateJWT() });
+        } else {
+            return res.status(401).json(info);
+        }
+    })(req, res, next);
 });
 
 module.exports = router;
