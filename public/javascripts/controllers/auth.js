5f0ec823a5b02f5cf21f57c246cdcab224412026
Eric Quanz <eric.quanz@gmail.com>
2015-12-12 15:30:20 -0200

    add authentication controller

---
+++ public/javascripts/controllers/auth.js
@@ -0,0 +1,23 @@
+angular.module('flapperNews').controller('AuthCtrl', [
+    '$scope',
+    '$state',
+    'auth',
+function($scope, $state, auth) {
+    $scope.user = {};
+
+    $scope.register = function() {
+        auth.register($scope.user).error(function(error) {
+            $scope.error = error;
+        }).then(function() {
+            $state.go('home');
+        });
+    }
+
+    $scope.login = function() {
+        auth.login($scope.user).error(function(error) {
+            $scope.error = error;
+        }).then(function() {
+            $state.go('home');
+        });
+    }
+}]);
